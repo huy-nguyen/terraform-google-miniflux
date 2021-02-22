c2b2035fdf5a72f8eae69aa96d74187e4d517e52
Huy Nguyen <me@huy.dev>
2021-02-21 21:30:17 -0500

    Add minimum version constraints for terraform & google providers

--- examples/minimal.tf
+++ examples/minimal.tf
@@ -1,14 +1,10 @@
-provider "google" {
-  credentials = file("path_to_file.json")
-  project     = "my-project-id"
-}
-
 module "miniflux" {
   source  = "huy-nguyen/miniflux/google"
   version = "4.0.0"
 
-  region = "us-east1"
-  zone   = "us-east1-d"
+  project_id = "my-project-id"
+  region     = "us-east1"
+  zone       = "us-east1-d"
   private_services_access_ip_range = {
     starting_address = "192.168.16.0"
     prefix_length    = 20
